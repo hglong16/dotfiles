0d0de5f3f29963af54f38d64ce76f94811bf32c5
hglong16 <intihad.vuong@gmail.com>
2022-01-18 12:04:27 +0700

    fish done

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,38 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+set fish_greeting ""
+set -gx TERM xterm-256color
+
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll - A"
+
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+
+set -gx PATH node_modules/.bin $PATH
+
+# GO
+
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
+
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
 end
---
+++ fish/functions/peco_change_directory.fish
@@ -0,0 +1,20 @@
+function _peco_change_directory
+  if [ (count $argv) ]
+    peco --layout=bottom-up --query "$argv "|perl -pe 's/([ ()])/\\\\$1/g'|read foo
+  else
+    peco --layout=bottom-up |perl -pe 's/([ ()])/\\\\$1/g'|read foo
+  end
+  if [ $foo ]
+    builtin cd $foo
+    commandline -r ''
+    commandline -f repaint
+  else
+    commandline ''
+  end
+end
+
+function peco_change_directory
+  begin
+    echo $HOME/.config
+  end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
+end
