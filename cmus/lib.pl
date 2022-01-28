915c0d236d0cf6a479c8fa1dc1e92e0aefbd294a
hglong16 <intihad.vuong@gmail.com>
2022-01-28 22:29:54 +0700

    minimal config

---
+++ cmus/lib.pl
@@ -0,0 +1,3 @@
+/Users/hglong/Desktop/Music/[STAY HOME, STAY HAPPY, STAY HÀANHTUẤN] Xuân Xuân - Hạ Hạ - Thu Thu - Đông Đông - Rồi Sẽ Lại Xuân-MMgPOQ9gJhM.mp4
+/Users/hglong/Desktop/Music/copyBest Relaxing Piano Studio Ghibli Complete Collection 2016-EDRmJSwrVns.webm
+/Users/hglong/Desktop/Music/📚4-HOUR STUDY WITH ME _ 🎹calm piano + gentle rain sound🌧 _ Tokyo at RAINY MORNING _ timer+bell-nF1fSKEiyA0.opus
