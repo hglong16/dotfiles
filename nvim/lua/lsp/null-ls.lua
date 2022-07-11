37e16db3834006ca12d077fc86662afede597564
hglong16 <intihad.vuong@gmail.com>
2022-07-11 22:12:51 +0700

    fix bug

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -2,54 +2,53 @@ local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local with_diagnostics_code = function(builtin)
-    return builtin.with({
-        diagnostics_format = "#{m} [#{c}]",
-    })
+  return builtin.with({
+    diagnostics_format = "#{m} [#{c}]",
+  })
 end
 
 local with_root_file = function(builtin, file)
-    return builtin.with({
-        condition = function(utils)
-            return utils.root_has_file(file)
-        end,
-    })
+  return builtin.with({
+    condition = function(utils)
+      return utils.root_has_file(file)
+    end,
+  })
 end
 
 local sources = {
-    -- formatting
-    b.formatting.prettier,
-    b.formatting.fish_indent,
-    b.formatting.shfmt,
-    b.formatting.rustfmt,
-    b.formatting.yapf,
-    b.formatting.stylua,
-    b.formatting.trim_whitespace.with({ filetypes = { "tmux", "teal" } }),
-    b.formatting.isort,
-    with_root_file(b.formatting.stylua, "stylua.toml"),
-    -- diagnostics
-    with_root_file(b.diagnostics.selene, "selene.toml"),
-    b.diagnostics.write_good,
-    b.diagnostics.luacheck,
-    b.diagnostics.flake8,
-    b.diagnostics.codespell,
-    b.diagnostics.markdownlint,
-    b.diagnostics.teal,
-    b.diagnostics.tsc,
-    with_diagnostics_code(b.diagnostics.shellcheck),
-    -- code actions
-    b.code_actions.gitsigns,
-    b.code_actions.gitrebase,
-    -- hover
-    b.hover.dictionary,
+  -- formatting
+  b.formatting.prettier,
+  b.formatting.fish_indent,
+  b.formatting.shfmt,
+  b.formatting.rustfmt,
+  b.formatting.yapf,
+  b.formatting.stylua,
+  b.formatting.trim_whitespace.with({ filetypes = { "tmux", "teal" } }),
+  b.formatting.isort,
+  with_root_file(b.formatting.stylua, "stylua.toml"),
+  -- diagnostics
+  with_root_file(b.diagnostics.selene, "selene.toml"),
+  b.diagnostics.write_good,
+  b.diagnostics.luacheck,
+  b.diagnostics.flake8,
+  b.diagnostics.markdownlint,
+  b.diagnostics.teal,
+  b.diagnostics.tsc,
+  with_diagnostics_code(b.diagnostics.shellcheck),
+  -- code actions
+  b.code_actions.gitsigns,
+  b.code_actions.gitrebase,
+  -- hover
+  b.hover.dictionary,
 }
 
 local M = {}
 M.setup = function(on_attach)
-    null_ls.setup({
-        -- debug = true,
-        sources = sources,
-        on_attach = on_attach,
-    })
+  null_ls.setup({
+    -- debug = true,
+    sources = sources,
+    on_attach = on_attach,
+  })
 end
 
 return M
